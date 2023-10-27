4c495a8647be29c4e2d68d472af7edfbdfba8a0e
Marco Luzzara <marco.luzzara@hotmail.it>
2023-10-28 00:59:37 +0200

    replace S3Client with Java Nio support. Wip: library bug to fix
    The library does not seem to be fully compatible with Java 17.

--- src/main/java/it/addvalue/demo/configurations/LambdaConfiguration.java
+++ src/main/java/it/addvalue/demo/configurations/LambdaConfiguration.java
@@ -4,19 +4,25 @@ import it.addvalue.demo.model.fill.FillInput;
 import it.addvalue.demo.model.fill.FillOutput;
 import it.addvalue.demo.model.process.ProcessInput;
 import it.addvalue.demo.model.process.ProcessOutput;
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.core.internal.http.loader.DefaultSdkHttpClientBuilder;
-import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.http.SdkHttpConfigurationOption;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.utils.AttributeMap;
+import software.amazon.nio.spi.s3.config.S3NioSpiConfiguration;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.spi.FileSystemProvider;
+import java.util.Map;
 import java.util.function.Function;
 
 @Configuration
@@ -24,42 +30,29 @@ public class LambdaConfiguration {
     @Bean
     public Function<FillInput, FillOutput> fill() {
         return (input) -> {
-            try (var s3Client = S3Client.builder()
-                    .forcePathStyle(true)
-                    .httpClient(getSdkHttpClient())
-                    .endpointOverride(URI.create("http://localstackmaindemo:4566"))
-                    .build()) {
+            try (var fs = getNewFileSystem(input.bucketName())) {
                 for (var i = 0; i < input.keyCount(); i++) {
-                    var objectRequest = PutObjectRequest.builder()
-                            .bucket(input.bucketName())
-                            .key(Integer.toString(i))
-                            .build();
-
-                    s3Client.putObject(objectRequest, RequestBody.fromBytes(new byte[] { (byte) i }));
+                    Files.write(
+                            fs.getPath(Integer.toString(i)),
+                            new byte[]{(byte) i});
                 }
-            }
 
-            return new FillOutput(input.bucketName());
+                return new FillOutput(input.bucketName());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         };
     }
 
     @Bean
     public Function<ProcessInput, ProcessOutput> process() {
         return (input) -> {
-            try (var s3Client = S3Client.builder()
-                    .forcePathStyle(true)
-                    .httpClient(getSdkHttpClient())
-                    .endpointOverride(URI.create("http://localstackmaindemo:4566"))
-                    .build()) {
-                var objectRequest = GetObjectRequest.builder()
-                        .bucket(input.bucketName())
-                        .key(input.key())
-                        .build();
-
-                var objectBytes = s3Client.getObjectAsBytes(objectRequest);
-                var keyContent = objectBytes.asByteArray();
+            try (var fs = getNewFileSystem(input.bucketName())) {
+                var keyContent = Files.readAllBytes(fs.getPath(input.key()));
 
                 return new ProcessOutput(new BigInteger(keyContent).intValue() * 2);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         };
     }
@@ -69,5 +62,15 @@ public class LambdaConfiguration {
                 .put(SdkHttpConfigurationOption.TRUST_ALL_CERTIFICATES, true)
                 .build();
         return new DefaultSdkHttpClientBuilder().buildWithDefaults(attributeMap);
+    }
+
+    private FileSystem getNewFileSystem(String bucketName) {
+        try {
+            return FileSystems.newFileSystem(URI.create("s3x://%s/%s".formatted("localstackmaindemo:4566", bucketName)),
+                    new S3NioSpiConfiguration(),
+                    Thread.currentThread().getContextClassLoader());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
